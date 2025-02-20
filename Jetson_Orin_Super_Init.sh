16b5445016ae49708046dfced01a2d3657a25eff
Snowolf <zlex.dongliang@gmail.com>
2025-02-20 14:56:05 +0800

    Create Jetson_Orin_Super_Init.sh

---
+++ Jetson_Orin_Super_Init.sh
@@ -0,0 +1,19 @@
+
+echo "下载内核"
+
+wget https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v4.3/release/Jetson_Linux_r36.4.3_aarch64.tbz2
+wget https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v4.3/release/Tegra_Linux_Sample-Root-Filesystem_r36.4.3_aarch64.tbz2
+
+echo "解压内核"
+tar xf Jetson_Linux_r36.4.3_aarch64.tbz2 
+sudo tar xpf Tegra_Linux_Sample-Root-Filesystem_R36.4.3_aarch64.tbz2 -C Linux_for_Tegra/rootfs/
+cd Linux_for_Tegra/
+
+echo ""
+sudo ./tools/l4t_flash_prerequisites.sh
+
+
+sudo ./apply_binaries.sh
+
+
+sudo ./tools/kernel_flash/l4t_initrd_flash.sh --external-device nvme0n1p1 -c tools/kernel_flash/flash_l4t_t234_nvme.xml -p "-c bootloader/generic/cfg/flash_t234_qspi.xml" --showlogs --network usb0 jetson-orin-nano-devkit-super internal
