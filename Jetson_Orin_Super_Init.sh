4faa3bf9e60bd3ec45f5ae979389e10ca0b020d4
Snowolf <zlex.dongliang@gmail.com>
2025-02-20 19:20:34 +0800

    Update Jetson_Orin_Super_Init.sh

--- Jetson_Orin_Super_Init.sh
+++ Jetson_Orin_Super_Init.sh
@@ -15,17 +15,19 @@ sudo ./tools/l4t_flash_prerequisites.sh
 echo "镜像文件准备"
 sudo ./apply_binaries.sh
 
+flash_to_ssd(){
 echo "系统烧录 SSD"
-
 sudo ./tools/kernel_flash/l4t_initrd_flash.sh \
   --external-device nvme0n1p1 \
   -c tools/kernel_flash/flash_l4t_t234_nvme.xml \
   -p "-c bootloader/generic/cfg/flash_t234_qspi.xml" \
   --showlogs --network usb0 jetson-orin-nano-devkit-super internal
+}
+
+flash_to_usb_disk(){
 
 echo "系统烧录-USB Disk"
 
-flash_to_UDisk(){
 sudo ./tools/kernel_flash/l4t_initrd_flash.sh \
   --external-device sda1 \
   -c tools/kernel_flash/flash_l4t_t234_nvme.xml \
